84268f4e13fb045a38c75056e2f27f1790e57e10
Mounirkheloufi <165382760+Mounirkheloufi@users.noreply.github.com>
2025-09-07 01:14:46 +0200

    Initialize backend with Express and basic setup

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
