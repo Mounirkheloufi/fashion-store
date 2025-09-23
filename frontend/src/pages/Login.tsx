f8574801491fb7fdfc3095ac00752cc5367420b6
Mounirkheloufi <165382760+Mounirkheloufi@users.noreply.github.com>
2025-09-23 23:52:05 +0200

    update login form

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,34 +1,62 @@
 import React from 'react'
-import { Form, Input, Button, Card } from 'antd'
+import { Form, Input, Button, Card, Checkbox, Typography } from 'antd'
 import { useDispatch } from 'react-redux'
 import { login } from '../store/userSlice'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 
+const { Text } = Typography
 
-export default function Login(){
-const dispatch = useDispatch()
-const navigate = useNavigate()
-const onFinish = async (values:any) => {
-try{
-await dispatch(login(values) as any)
-navigate('/')
-}catch(e){ console.error(e) }
-}
+export default function Login() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
+  const onFinish = async (values: any) => {
+    try {
+      await dispatch(login(values) as any)
+      if (values.remember) {
+        localStorage.setItem("userEmail", values.email)
+      }
+      navigate('/')
+    } catch (e) {
+      console.error(e)
+    }
+  }
 
-return (
-<Card style={{ maxWidth:400, margin:'40px auto' }}>
-<Form onFinish={onFinish} layout="vertical">
-<Form.Item label="Email" name="email" rules={[{ required:true }]}>
-<Input />
-</Form.Item>
-<Form.Item label="Password" name="password" rules={[{ required:true }]}>
-<Input.Password />
-</Form.Item>
-<Form.Item>
-<Button type="primary" htmlType="submit" block>Login</Button>
-</Form.Item>
-</Form>
-</Card>
-)
+  return (
+    <Card style={{ maxWidth: 400, margin: '40px auto', padding: '20px' }}>
+      <Form onFinish={onFinish} layout="vertical" initialValues={{ remember: true }}>
+        
+        {/* Email */}
+        <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Email is required' }]}>
+          <Input />
+        </Form.Item>
+
+        {/* Password */}
+        <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Password is required' }]}>
+          <Input.Password />
+        </Form.Item>
+
+        {/* Remember me + Forgot password */}
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+        <Link to="/forgot-password" style={{ float: 'right' }}>
+          Forgot password?
+        </Link>
+
+        {/* Submit */}
+        <Form.Item style={{ marginTop: 20 }}>
+          <Button type="primary" htmlType="submit" block>
+            Login
+          </Button>
+        </Form.Item>
+
+        {/* Sign up */}
+        <div style={{ textAlign: 'center', marginTop: 10 }}>
+          <Text>Don't have an account? </Text>
+          <Link to="/register">Sign up</Link>
+        </div>
+      </Form>
+    </Card>
+  )
 }
