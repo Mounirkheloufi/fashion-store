03151f6213d063d37802c5d86e23891eee55d5b2
Mounirkheloufi <165382760+Mounirkheloufi@users.noreply.github.com>
2025-10-03 12:20:17 +0200

    fix userModel

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -23,9 +23,25 @@ async function findUserById(id) {
     return rows[0];
 }
 
+async function updateUserProfile(id, { email, password, profile_picture }) {
+    const [result] = await pool.query(
+        "UPDATE users SET email = ?, password = ?, profile_picture = ? WHERE id = ?",
+        [email, password, profile_picture, id]
+    );
+    return result.affectedRows > 0;
+}
+
+
+async function deleteUser(id) {
+    const [result] = await pool.query("DELETE FROM users WHERE id = ?", [id]);
+    return result.affectedRows > 0;
+}
+
 module.exports = {
     getAllUsers,
     createUser,
     findUserByEmail,
-    findUserById
+    findUserById,
+    updateUserProfile,
+    deleteUser
 };
