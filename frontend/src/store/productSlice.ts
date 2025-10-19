eb9ff2296b827d12d7d30570b3f8caecf6ed88ca
Mounirkheloufi <165382760+Mounirkheloufi@users.noreply.github.com>
2025-10-19 22:16:39 +0200

    update: product slice

--- frontend/src/store/productSlice.ts
+++ frontend/src/store/productSlice.ts
@@ -26,7 +26,7 @@ const initialState: ProductState = {
 
 // --- Thunks ---
 export const fetchProducts = createAsyncThunk("products/fetchAll", async () => {
-  const res = await axios.get<Product[]>("/api/products");
+  const res = await axios.get("http://localhost:5000/api/products");
   return res.data;
 });
 
